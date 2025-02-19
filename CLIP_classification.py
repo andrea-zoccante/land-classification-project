a3c12226064ebfd36621deba5e0ac15f2fe1c0ca
ianblair87 <ianwilliamblair@gmail.com>
2025-02-19 14:17:48 +0100

    conda environment

--- CLIP_classification.py
+++ CLIP_classification.py
@@ -1,1 +1,18 @@
-print("Hello Andrea")
+from PIL import Image
+import requests
+
+from transformers import CLIPProcessor, CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+# data_dir = "./2750/"
+im = Image.open("./2750/Pasture/Pasture_100.jpg")
+
+inputs = processor(text=["a satelite image of farmland", "a satelite image of a forest"], images=im, return_tensors="pt", padding=True)
+
+outputs = model(**inputs)
+logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
+probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
+
+print(probs)
